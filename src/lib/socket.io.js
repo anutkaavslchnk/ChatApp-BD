f669073632045215e5b0e779056bcf0d48e3bcb6
Anna Vasylchenko <anutochkaaaaa14@gmail.com>
2025-07-11 18:41:51 +0200

    changes

--- src/lib/socket.io.js
+++ src/lib/socket.io.js
@@ -1,116 +1,101 @@
-import {Server} from 'socket.io';
+import { Server } from "socket.io";
 
-import http from 'http';
-import express from 'express';
+import http from "http";
+import express from "express";
 
+const app = express();
+const server = http.createServer(app);
 
-const app=express();
-const server=http.createServer(app);
-
-const io=new Server(server, {
-    cors:{
-        origin:["http://localhost:5173"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: ["https://chat-app-five-navy-48.vercel.app/"],
+  },
 });
 
-export const  getReceiverId=(userId)=>{
-    return userSocketMap[userId]
-}
-
-
+export const getReceiverId = (userId) => {
+  return userSocketMap[userId];
+};
 
 //store online users
-const userSocketMap={};
+const userSocketMap = {};
 
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
 
-    io.on('connection', (socket)=>{
-    console.log("A user connected", socket.id);
+  socket.on("typing", ({ to, from }) => {
+    const receiverSocketId = userSocketMap[to];
+    if (receiverSocketId) {
+      socket.to(receiverSocketId).emit("typing", { from });
+    }
+  });
 
-    socket.on("typing", ({to, from})=>{
-        const receiverSocketId = userSocketMap[to];
-  if (receiverSocketId) {
-    socket.to(receiverSocketId).emit("typing", { from });
+  socket.on("deleteMessage", ({ messageId, senderId, receiverId }) => {
+    console.log("Socket deleteMessage received:", messageId);
+
+    const senderSocketId = userSocketMap[senderId];
+    const receiverSocketId = userSocketMap[receiverId];
+
+    if (senderSocketId === socket.id) {
+      socket.emit("messageDeleted", { messageId });
+    } else if (senderSocketId) {
+      io.to(senderSocketId).emit("messageDeleted", { messageId });
+    }
+
+    if (receiverSocketId && receiverSocketId !== senderSocketId) {
+      io.to(receiverSocketId).emit("messageDeleted", { messageId });
+    }
+  });
+
+  socket.on("updateMessage", ({ messageId, senderId, receiverId, newText }) => {
+    console.log("Socket updateMessage received:", messageId, newText);
+
+    const senderSocketId = userSocketMap[senderId];
+    const receiverSocketId = userSocketMap[receiverId];
+
+    const updatedPayload = { messageId, newText };
+
+    if (senderSocketId) {
+      io.to(senderSocketId).emit("messageUpdated", updatedPayload);
+    }
+
+    if (receiverSocketId && receiverSocketId !== senderSocketId) {
+      io.to(receiverSocketId).emit("messageUpdated", updatedPayload);
+    }
+  });
+
+  socket.on("stopTyping", ({ to, from }) => {
+    const receiverSocketId = userSocketMap[to];
+    if (receiverSocketId) {
+      socket.to(receiverSocketId).emit("stopTyping", { from });
+    }
+  });
+
+  socket.on("messageDelivered", ({ messageId, senderId }) => {
+    const senderSocketId = userSocketMap[senderId];
+    if (senderSocketId) {
+      socket.to(senderSocketId).emit("messageDelivered", { messageId });
+    }
+  });
+  socket.on("messageRead", ({ messageId, senderId }) => {
+    const senderSocketId = userSocketMap[senderId];
+    if (senderSocketId) {
+      socket.to(senderSocketId).emit("messageRead", { messageId });
+    }
+  });
+  const userId = socket.handshake.query.userId;
+
+  if (userId) {
+    userSocketMap[userId] = socket.id;
   }
-        });
 
-socket.on('deleteMessage', ({ messageId, senderId, receiverId }) => {
-  console.log('Socket deleteMessage received:', messageId);
+  //is used to send events to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-  const senderSocketId = userSocketMap[senderId];
-  const receiverSocketId = userSocketMap[receiverId];
-
-
-  if (senderSocketId === socket.id) {
-    socket.emit('messageDeleted', { messageId }); 
-  } else if (senderSocketId) {
-    io.to(senderSocketId).emit('messageDeleted', { messageId }); 
-  }
-
-
-  if (receiverSocketId && receiverSocketId !== senderSocketId) {
-    io.to(receiverSocketId).emit('messageDeleted', { messageId });
-  }
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
 });
-      
-      
-socket.on('updateMessage', ({ messageId, senderId, receiverId, newText }) => {
-  console.log('Socket updateMessage received:', messageId, newText);
 
-  const senderSocketId = userSocketMap[senderId];
-  const receiverSocketId = userSocketMap[receiverId];
-
-  
-  const updatedPayload = { messageId, newText };
-
-  if (senderSocketId) {
-    io.to(senderSocketId).emit('messageUpdated', updatedPayload);
-  }
-
-  if (receiverSocketId && receiverSocketId !== senderSocketId) {
-    io.to(receiverSocketId).emit('messageUpdated', updatedPayload);
-  }
-});
-
-        
-        socket.on("stopTyping", ({to, from})=>{
-            const receiverSocketId = userSocketMap[to];
-            if (receiverSocketId) {
-              socket.to(receiverSocketId).emit("stopTyping", { from });
-            }
-            })
-
-            socket.on("messageDelivered", ({messageId, senderId})=>{
-                const senderSocketId=userSocketMap[senderId];
-                if(senderSocketId){
-                    socket.to(senderSocketId).emit("messageDelivered", {messageId});
-                }
-            });
-            socket.on("messageRead", ({messageId,senderId})=>{
-                const senderSocketId=userSocketMap[senderId];
-                if(senderSocketId){
-                    socket.to(senderSocketId).emit("messageRead", {messageId});
-                }
-            })
-    const userId=socket.handshake.query.userId;
-
-if (userId){
-
-    userSocketMap[userId]=socket.id;
-
-} 
-
-//is used to send events to all connected clients
-io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-
-    socket.on("disconnect", ()=>{
-        console.log("A user disconnected", socket.id);
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers", Object.keys(userSocketMap))
-       
-        
-    })
-    
-})
-
-export {io, app, server};
+export { io, app, server };
