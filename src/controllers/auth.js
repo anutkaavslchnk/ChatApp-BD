c962a7407040faaf9a6de2168debf1c75359c9bd
Anna Vasylchenko <anutochkaaaaa14@gmail.com>
2025-07-20 20:23:47 +0200

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,63 +1,69 @@
-
 import { ONE_DAY } from "../constants/constans.js";
-import { loginUser, logOutUser, refreshUsersSession, signUpUser, updateProfile } from "../services/auth.js";
+import {
+  loginUser,
+  logOutUser,
+  refreshUsersSession,
+  signUpUser,
+  updateProfile,
+} from "../services/auth.js";
 
 export const signUpController = async (req, res) => {
+  const userData = await signUpUser(req.body);
 
-
-      const userData = await signUpUser(req.body);
-  
-      res.json(userData);
-
-  };
-  
-
-  export const loginController = async (req, res) => {
-const {session,user}=await loginUser(req.body);
-res.cookie('refreshToken', session.refreshToken,{
-  httpOnly:true,
-  expires: new Date(Date.now()+ONE_DAY),
-});
-res.cookie('sessionId', session._id,{
-  httpOnly:true,
-  expires: new Date(Date.now()+ONE_DAY),
-});
-
-res.json({
-  user,
-  accessToken:session.accessToken,
-})
-  };
-  
-  
-
-  export const logoutController=async(req,res)=>{
-    const { sessionId } = req.cookies;
-    if (!sessionId) {
-        return res.status(400).json({
-          status: 400,
-          message: "Session ID is missing in cookies",
-        });
-      }
-if(sessionId){
-    await logOutUser(sessionId);
-}
-res.clearCookie('sessionId');
-res.clearCookie('refreshToken');
-res.status(204).send();
+  res.json(userData);
 };
-  
 
-const setupSession=(res,session)=>{
-  res.cookie('refreshToken', session.refreshToken,{
-    httpOnly:true,
-    expires: new Date(Date.now()+ONE_DAY),
+export const loginController = async (req, res) => {
+  const { session, user } = await loginUser(req.body);
+  res.cookie("refreshToken", session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+    sameSite: "None",
+    secure: true,
   });
-  res.cookie('sessionId', session._id,{
-    httpOnly:true,
-    expires: new Date(Date.now()+ONE_DAY),
+  res.cookie("sessionId", session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+    sameSite: "None",
+    secure: true,
   });
-}
+
+  res.json({
+    user,
+    accessToken: session.accessToken,
+  });
+};
+
+export const logoutController = async (req, res) => {
+  const { sessionId } = req.cookies;
+  if (!sessionId) {
+    return res.status(400).json({
+      status: 400,
+      message: "Session ID is missing in cookies",
+    });
+  }
+  if (sessionId) {
+    await logOutUser(sessionId);
+  }
+  res.clearCookie("sessionId");
+  res.clearCookie("refreshToken");
+  res.status(204).send();
+};
+
+const setupSession = (res, session) => {
+  res.cookie("refreshToken", session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+    sameSite: "None",
+    secure: true,
+  });
+  res.cookie("sessionId", session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+    sameSite: "None",
+    secure: true,
+  });
+};
 
 export const refreshUserSessionController = async (req, res) => {
   const session = await refreshUsersSession({
@@ -69,31 +75,22 @@ export const refreshUserSessionController = async (req, res) => {
 
   res.json({
     status: 200,
-    message: 'Successfully refreshed a session!',
+    message: "Successfully refreshed a session!",
     data: {
       accessToken: session.accessToken,
     },
   });
 };
 
-export const updateProfileController=async(req,res,next)=>{
-  await updateProfile(req, res,next);
-   
+export const updateProfileController = async (req, res, next) => {
+  await updateProfile(req, res, next);
+};
 
-}
-
-
-
-export const checkController=(req,res)=>{
-try {
-  res.status(200).json(req.user);
-} catch (error) {
-  console.log(error.message);
-  res.status(500).json({message:"Internal server error"});
-  
-}
-   
-
-}
-
-
+export const checkController = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
